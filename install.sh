d2857fb050fe0477bc48dd812efa4d1cb6ecd860
Jeremy Whittemore <noreply@github.com>
2025-10-26 13:37:58 -0400

    add docker

--- install.sh
+++ install.sh
@@ -2,7 +2,20 @@
 
 set -ex
 
-sudo apt update && sudo apt install software-properties-common
-sudo add-apt-repository --yes --update ppa:ansible/ansible   
-sudo apt install ansible -y   
+sudo apt update && sudo apt install -y \
+	curl \
+	apt-transport-https \
+	ca-certificates \
+	software-properties-common \
+	git \
+	vim \
+	wget
 
+# install docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo docker run hello-world
+sudo usermod -aG docker $USER
