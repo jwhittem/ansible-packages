3cd824ad0e9de7cb3928767a59137e9b1fd6b52c
Jeremy Whittemore <noreply@github.com>
2025-10-26 14:06:06 -0400

    update install script

--- install.sh
+++ install.sh
@@ -13,9 +13,9 @@ sudo apt update && sudo apt install -y \
 
 
 # install docker
+mkdir /etc/docker || echo "Docker config folder exists"
 
-echo "{ "data-root": "$HOME/.docker" }" | sudo tee /etc/docker/daemon.json
-
+echo '{ "data-root": "/home/docker" }' | sudo tee /etc/docker/daemon.json
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
@@ -24,4 +24,3 @@ sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin d
 sudo docker run hello-world
 sudo usermod -aG docker $USER
 
-echo "{ "data-root": "$HOME/.docker" }"
